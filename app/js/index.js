13b1b3fe9c4525ac92a7bb6ba2e94c9ce7d71ea4
retrong <eguobabarth@gmail.com>
2023-01-07 01:37:11 +0100

    final commit

--- app/js/index.js
+++ app/js/index.js
@@ -1,74 +1,92 @@
-// get the form element
-const numberOfCard = document.querySelector(".cardnumber p");
-const nameOfCard = document.querySelector(".name p");
-const dateOfCard = document.querySelector(".date p");
-const cvcOfCard = document.querySelector(".zeros p");
-const form = document.forms[0];
-const formContainer = document.querySelector(".form");
-const final = document.querySelector(".final");
-const backHome = document.querySelector(".final button");
-const fields = Array.from(document.querySelectorAll(".required"));
+const cardNumber = document.getElementById("number");
+const numberInp = document.getElementById("card_number");
+const nameInp = document.getElementById("card_name");
+const cardName = document.getElementById("name");
+const cardMonth = document.getElementById("month");
+const cardYear = document.getElementById("year");
+const monthInp = document.getElementById("card_month");
+const yearInp = document.getElementById("card_year");
+const cardCvc = document.getElementById("cvc");
+const cvcInp = document.getElementById("card_cvc");
+const submitBtn = document.getElementById("submit_btn");
+const completed = document.querySelector(".thank");
+const form = document.querySelector("form");
 
-form.addEventListener("submit", function(e) {
-    const cardName = form.elements.cardholder;
-    const cardNumber = form.elements.cardnumber;
-    const month = form.elements.month;
-    const year = form.elements.year;
-    const cvc = form.elements.cvc;
-    if(cardNumber.value.length != 16) {
-        e.preventDefault();
-        cardNumber.nextElementSibling.textContent = "Check length!";
-
-    } else if(month.value.length != 2) {
-        cardNumber.nextElementSibling.textContent = "";
-        e.preventDefault();
-        month.nextElementSibling.textContent = "Check length!";
-    } else if (+month.value < 0 || +month.value > 13) {
-        e.preventDefault();
-        month.nextElementSibling.textContent = "unknown month!";
-
-    } else if(year.value.length != 2) {
-        month.nextElementSibling.textContent = "";
-        e.preventDefault();
-        year.nextElementSibling.textContent = "Check length!";
-    }else if (+year.value < 22 || +year.value > 27) {
-        e.preventDefault();
-        month.nextElementSibling.textContent = "unvalid year!";
-
-    } else if(cvc.value.length != 3) {
-        year.nextElementSibling.textContent = "";
-        e.preventDefault();
-        cvc.nextElementSibling.textContent = "Check length!";
-    } else {
-        cvc.nextElementSibling.textContent = "";
-        formContainer.style.display = "none";
-        completed.style.display = "block";
-        nameOfCard.textContent = cardName.value;
-        numberOfCard.textContent = cardNumber.value;
-        dateOfCard.textContent = month.value + "/" + year.value;
-        cvcOfCard.textContent = cvc.value;
-        console.log(cardName.value.length, cardNumber.value.length, month.value.length + "/" + year.value.length, cvc.value.length);
-    }
-    e.preventDefault();
-    
-    
-
-});
-
-backHome.addEventListener("click", function(){
-    window.location.reload();
-});
-// remove error message
-
-
-for(let field of fields) {
-    field.addEventListener("change", function(){
-        if(field.value == "") {
-            this.nextElementSibling.textContent = "Can't be empty";
-        } else if(isNaN(field.value)) {
-            this.nextElementSibling.textContent = "Wrong format, numbers only";
-        } else {
-            this.nextElementSibling.textContent = "";
-        }
-    });
+function setCardNumber(e) {
+    cardNumber.innerText = format(e.target.value);
 }
+function setCardName(e) {
+  cardName.innerText = format(e.target.value);
+}
+function setCardMonth(e) {
+  cardMonth.innerText = format(e.target.value);
+}
+function setCardYear(e) {
+  cardYear.innerText = format(e.target.value);
+}
+function setCardCvc(e) {
+  cardCvc.innerText = format(e.target.value);
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    if (!nameInp.value) {
+      nameInp.classList.add('error');
+      nameInp.parentElement.classList.add("error_message")
+    } else {
+      nameInp.classList.remove("error");
+      nameInp.parentElement.classList.remove("error_message");
+    }
+    if (!numberInp.value) {
+      numberInp.classList.add('error')
+      numberInp.parentElement.classList.add("error_message");
+    } else if (numberInp.value.length < 16) {
+        numberInp.classList.add("error")
+    } else {
+      numberInp.classList.remove("error");
+      numberInp.parentElement.classList.remove("error_message");
+    }
+    if (!monthInp.value) {
+      monthInp.classList.add("error")
+      monthInp.parentElement.classList.add("error_message");
+    } else {
+      monthInp.classList.remove("error");
+      monthInp.parentElement.classList.remove("error_message");
+    }
+    if (!yearInp.value) {
+      yearInp.classList.add("error")
+      yearInp.parentElement.classList.add("error_message");
+    } else {
+      yearInp.classList.remove("error");
+      yearInp.parentElement.classList.remove("error_message");
+    }
+    if (!cvcInp.value) {
+      cvcInp.classList.add("error")
+      cvcInp.parentElement.classList.add("error_message");
+    } else {
+      cvcInp.classList.remove("error");
+      cvcInp.parentElement.classList.remove("error_message");
+    }
+    if (
+      nameInp.value &&
+      numberInp.value &&
+      monthInp.value &&
+      yearInp.value &&
+      cvcInp.value &&
+      numberInp.value.length == 16
+    ) {
+      completed.classList.remove("hidden");
+      form.classList.add("hidden");
+    }
+  
+}
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$& ");
+}
+
+numberInp.addEventListener("keyup", setCardNumber);
+nameInp.addEventListener("keyup", setCardName);
+monthInp.addEventListener("keyup", setCardMonth);
+yearInp.addEventListener("keyup", setCardYear);
+cvcInp.addEventListener("keyup", setCardCvc);
+submitBtn.addEventListener("click", handleSubmit);
